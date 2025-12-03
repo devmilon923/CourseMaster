11fcdb5d5f16c841ea57120a05425579856c28a5
Milon Mia <milonmia@Milons-iMac-Pro.local>
2025-12-03 18:57:39 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://course-master-frontend-eosin.vercel.app",
+    ],
     credentials: true,
   })
 );
